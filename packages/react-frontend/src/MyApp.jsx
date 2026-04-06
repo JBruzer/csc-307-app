55908d54f99919679533bae6858e033bc275f913
JBruzer <jbruzgul@calpoly.edu>
2026-04-06 11:34:08 -0700

    Delete Function

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import Table from "./Table";
+import {useState} from "react";
+
+
+function MyApp() {
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      name: "Dee",
+      job: "Aspiring actress"
+    },
+    {
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
+  return (
+  <div className="container">
+    <Table
+      characterData={characters}
+      removeCharacter={removeOneCharacter}
+    />
+  </div>
+  );
+}
+
+export default MyApp;
